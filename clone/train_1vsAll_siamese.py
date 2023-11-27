e8f600f168c63bdb62c8b8fc5badce6b974accdf
Konstantinos <konstantinos.kitsios@uzh.ch>
2023-11-27 20:55:58 +0100

    Relatively good version

--- clone/train_1vsAll_siamese.py
+++ clone/train_1vsAll_siamese.py
@@ -11,7 +11,7 @@ warnings.filterwarnings('ignore')
 import sys
 
 margin = 50
-lr = 0.0001
+lr = 1e-3
 print("Margin=%d" % margin)
 print("lr=%0.5f"% lr)
 
@@ -32,6 +32,79 @@ def contrastive_loss(embedding1, embedding2, label, margin=1.0):
                       label * 0.5 * torch.pow(torch.clamp(margin - distance, min=0.0), 2))
     # print(distance) # Should not vanish to zero
     return loss
+
+def eval_model(model, test_data_t):
+    model.eval()
+    similarity_scores = []
+    trues = []
+    iTest = 0
+    
+    while iTest < len(test_data_t):
+        batch = get_batch(test_data_t, iTest, BATCH_SIZE)
+        iTest += BATCH_SIZE
+        test1_inputs, test2_inputs, test_labels = batch
+        if USE_GPU:
+            test_labels = test_labels.cuda()
+    
+        model.batch_size = len(test_labels)
+        model.hidden = model.init_hidden()
+        with torch.no_grad():
+            embeddings1 = model(test1_inputs)
+            embeddings2 = model(test2_inputs)
+        similarity_score = torch.nn.functional.cosine_similarity(embeddings1, embeddings2)
+    
+        loss = contrastive_loss(embeddings1, embeddings2, Variable(test_labels), margin=margin)
+    
+        # calc testing acc
+        similarity_scores.extend(similarity_score.cpu())
+        trues.extend(1 - test_labels.cpu().numpy())
+    
+    
+    trues = np.array(trues)
+    
+
+
+    max_Acc = -np.inf
+    for similarity_threshold_int in range(-5, 10):
+        similarity_threshold = similarity_threshold_int/10
+        # Classify code pairs based on the similarity score and threshold
+        predicted_labels = (np.array(similarity_scores) > similarity_threshold)
+        acc = 1-np.sum(np.abs(predicted_labels-np.transpose(trues)))/trues.shape[0]
+        P, R, F1, _ = precision_recall_fscore_support(predicted_labels, trues, average='binary', pos_label=1)
+    
+    
+        if acc > max_Acc:
+            max_Acc = acc
+            best_similarity_threshold = similarity_threshold
+
+    predicted_labels = np.array(similarity_scores) > best_similarity_threshold
+    p, r, f, _ = precision_recall_fscore_support(trues, predicted_labels, average='binary')
+    acc = 1-np.sum(np.abs(predicted_labels-np.transpose(trues)))/trues.shape[0]
+    print("F1=%.3f, P=%.3f, R=%.3f, A=%.3f for similarity threshold %0.2f" % (f, p, r, acc, best_similarity_threshold))
+
+
+    max_F1 = -np.inf
+    for similarity_threshold_int in range(-5, 10):
+        similarity_threshold = similarity_threshold_int/10
+        # Classify code pairs based on the similarity score and threshold
+        predicted_labels = (np.array(similarity_scores) > similarity_threshold)
+        acc = 1-np.sum(np.abs(predicted_labels-np.transpose(trues)))/trues.shape[0]
+        P, R, F1, _ = precision_recall_fscore_support(predicted_labels, trues, average='binary', pos_label=1)
+    
+    
+        if F1 > max_F1:
+            max_F1 = F1
+            best_similarity_threshold = similarity_threshold
+            
+    predicted_labels = np.array(similarity_scores) > best_similarity_threshold
+    p, r, f, _ = precision_recall_fscore_support(trues, predicted_labels, average='binary')
+    acc = 1-np.sum(np.abs(predicted_labels-np.transpose(trues)))/trues.shape[0]
+    print("F1=%.3f, P=%.3f, R=%.3f, A=%.3f for similarity threshold %0.2f" % (f, p, r, acc, best_similarity_threshold))
+
+
+    sys.stdout.flush()
+    model.train()
+    return f, similarity_scores
 
 if __name__ == '__main__':
     import argparse
@@ -65,8 +138,6 @@ if __name__ == '__main__':
     BATCH_SIZE = 32
     USE_GPU = torch.cuda.is_available()
     print(USE_GPU)
-    #print(train_data)
-    precision, recall, f1 = 0, 0, 0
     print('Start training...')
     sys.stdout.flush()
     
@@ -98,19 +169,15 @@ if __name__ == '__main__':
         # training procedure
         prev_epoch_f1 = 0
         for epoch in range(EPOCHS):
-            print(epoch)
             model.train()
+            print(epoch)
             sys.stdout.flush()
 
             start_time = time.time()
             # training epoch
-            total_acc = 0.0
-            total_loss = 0.0
-            total = 0.0
             i = 0
             while i < len(train_data_t):
                 batch = get_batch(train_data_t, i, BATCH_SIZE)
-                i += BATCH_SIZE
                 train1_inputs, train2_inputs, train_labels = batch
 
                 if USE_GPU:
@@ -127,67 +194,31 @@ if __name__ == '__main__':
                 loss.backward()
                 optimizer.step()
 
-                # if (i/BATCH_SIZE) % 50 == 0:
-                #     print(loss)
-                #     print(torch.nn.functional.pairwise_distance(embeddings1, embeddings2))
+                if (i/BATCH_SIZE) % 100 == 0:
+                    print(i/BATCH_SIZE)
+                    print(loss.detach().numpy())
+                    t1 = torch.nn.functional.pairwise_distance(embeddings1, embeddings2).detach().numpy()
+                    t2 = train_labels.cpu()
 
-            ###### Start testing
-            similarity_scores = []
-            trues = []
-            total_loss = 0.0
-            total = 0.0
-            i = 0
+                    idx_clones = (t2 == 0).squeeze()
+                    idx_non_clones = (t2 == 1).squeeze()
+                    print("Clones:")
+                    #print(t1[idx_clones])
+                    print(t1[idx_clones].mean())
+                    print("Non clones:")
+                    #print(t1[idx_non_clones])
+                    print(t1[idx_non_clones].mean())
 
-            model.eval()
-            while i < len(test_data_t):
-                batch = get_batch(test_data_t, i, BATCH_SIZE)
+                    f, similarity_scores = eval_model(model, test_data_t)
+                    print()
                 i += BATCH_SIZE
-                test1_inputs, test2_inputs, test_labels = batch
-                if USE_GPU:
-                    test_labels = test_labels.cuda()
-
-                model.batch_size = len(test_labels)
-                model.hidden = model.init_hidden()
-                #output = model(test1_inputs, test2_inputs)
-                with torch.no_grad():
-                    embeddings1 = model(test1_inputs)
-                    embeddings2 = model(test2_inputs)
-                similarity_score = torch.nn.functional.cosine_similarity(embeddings1, embeddings2)
-
-                loss = contrastive_loss(embeddings1, embeddings2, Variable(test_labels))
-
-                # calc testing acc
-                #predicted = (output.data > 0.5).cpu().numpy()
-                similarity_scores.extend(similarity_score.cpu())
-                trues.extend(1 - test_labels.cpu().numpy())
-                total += len(test_labels)
-                total_loss += loss.item() * len(test_labels)
-
             
-            trues = np.array(trues)
-            max_F1 = -np.inf
-            for similarity_threshold_int in range(-5, 10):
-                similarity_threshold = similarity_threshold_int/10
-                # Classify code pairs based on the similarity score and threshold
-                predicted_labels = (np.array(similarity_scores) > similarity_threshold)
-                #acc = 1-np.sum(np.abs(predicted_labels-true_labels))/true_labels.shape[0]
-                P, R, F1, _ = precision_recall_fscore_support(predicted_labels, trues, average='binary', pos_label=1)
 
-
-                if F1 > max_F1:
-                    max_F1 = F1
-                    best_similarity_threshold = similarity_threshold
-
-            predicted_labels = np.array(similarity_scores) > best_similarity_threshold
-            p, r, f, _ = precision_recall_fscore_support(trues, predicted_labels, average='binary')
-            acc = 1-np.sum(np.abs(predicted_labels-np.transpose(trues)))/trues.shape[0]
-
-
-            if f<prev_epoch_f1:
+            f, similarity_scores = eval_model(model, test_data_t)
+            if f<=prev_epoch_f1:
                 print("Lower F1 than previous epoch. Early stopping...")
                 sys.stdout.flush()
                 break
             else:
-                print("(F1,P,R,A):%.3f, %.3f, %.3f, %.3f for similarity threshold %0.2f" % (f, p, r, acc, best_similarity_threshold))
                 sys.stdout.flush()
                 prev_epoch_f1 = f
