a50723966a6427823e0e8f4edc1de91ef0c1ca3a
Konstantinos <konstantinos.kitsios@uzh.ch>
2023-11-29 10:39:38 +0100

    Added scripts for BCB-->SCB and SCB-->BCB experiments

---
+++ clone/bcb_scb_baseline.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import torch
+import time
+import numpy as np
+import warnings
+from gensim.models.word2vec import Word2Vec
+from model import BatchProgramCC
+from torch.autograd import Variable
+from sklearn.metrics import precision_recall_fscore_support
+import sys
+import argparse
+import matplotlib.pyplot as plt
+from my_utilities import get_batch, eval_model_baseline
+
+mode           = "trainBCBtestSCB" # either "trainBCBtestSCB" or "trainSCBtestBCB"
+lang           = "java"
+HIDDEN_DIM     = 100
+ENCODE_DIM     = 128
+LABELS         = 1
+EPOCHS         = 5
+BATCH_SIZE     = 32
+early_stopping = False
+USE_GPU        = torch.cuda.is_available()
+
+assert mode in ["trainBCBtestSCB", "trainSCBtestBCB"]
+
+print("Mode=%s" % mode)
+
+root   = 'data/'
+data_bcb_and_scb = pd.read_pickle(root+lang+'/scb/blocks.pickle').sample(frac=1)
+
+word2vec = Word2Vec.load(root+lang+"/scb/embedding/node_w2v_128").wv
+MAX_TOKENS = word2vec.vectors.shape[0]
+EMBEDDING_DIM = word2vec.vectors.shape[1]
+embeddings = np.zeros((MAX_TOKENS + 1, EMBEDDING_DIM), dtype="float32")
+embeddings[:word2vec.vectors.shape[0]] = word2vec.vectors
+
+# Initialize model
+model = BatchProgramCC(EMBEDDING_DIM,HIDDEN_DIM,MAX_TOKENS+1,ENCODE_DIM,LABELS,BATCH_SIZE,
+                            USE_GPU, embeddings)
+if USE_GPU:
+    model.cuda()
+
+parameters = model.parameters()
+#optimizer = torch.optim.Adamax(parameters, lr=0.0001)
+optimizer = torch.optim.Adam(parameters, lr=1e-3)
+loss_function = torch.nn.BCELoss()
+
+# SCB has NaN in functionality_ID
+if mode == "trainBCBtestSCB":
+    data_train = data_bcb_and_scb[~data_bcb_and_scb['functionality_id'].isna()]
+    
+    data_test = data_bcb_and_scb[data_bcb_and_scb['functionality_id'].isna()]
+elif mode == "trainSCBtestBCB":
+    data_train = data_bcb_and_scb[data_bcb_and_scb['functionality_id'].isna()]
+    
+    data_test = data_bcb_and_scb[~data_bcb_and_scb['functionality_id'].isna()]
+
+
+for epoch in range(EPOCHS):
+    print("Starting epoch %d" % epoch)
+    sys.stdout.flush()
+    i = 0
+    while i < len(data_train):
+        model.train()
+        batch = get_batch(data_train, i, BATCH_SIZE)
+        train1_inputs, train2_inputs, train_labels = batch
+        if USE_GPU:
+            train1_inputs, train2_inputs, train_labels = train1_inputs, train2_inputs, train_labels.cuda()
+    
+        model.zero_grad()
+        model.batch_size = len(train_labels)
+        model.hidden     = model.init_hidden()
+    
+        output = model(train1_inputs, train2_inputs)
+    
+        loss = loss_function(output, Variable(train_labels))
+        loss.backward()
+        optimizer.step()
+        i += BATCH_SIZE
+
+    
+    print("Starting evaluation after epoch %d" % epoch)
+    sys.stdout.flush()
+    f, similarity_scores = eval_model_baseline(model, data_test, BATCH_SIZE, USE_GPU)
+    sys.stdout.flush()
+
+    if early_stopping and f<prev_epoch_f1:
+        print("Lower F1 than previous epoch. Early stopping...")
+        sys.stdout.flush()
+        break
+    else:
+        prev_epoch_f1 = f
---
+++ clone/my_utilities.py
@@ -0,0 +1,83 @@
+import torch
+import numpy as np
+from sklearn.metrics import precision_recall_fscore_support
+import sys
+
+def get_batch(dataset, idx, bs):
+    tmp = dataset.iloc[idx: idx+bs]
+    x1, x2, labels = [], [], []
+    for _, item in tmp.iterrows():
+        x1.append(item['code_x'])
+        x2.append(item['code_y'])
+        labels.append([item['label']])
+    return x1, x2, torch.FloatTensor(labels)
+
+
+def eval_model_baseline(model, test_data_t, batch_size, use_gpu):
+    model.eval()
+    similarity_scores = []
+    trues = []
+    iTest = 0
+    
+    while iTest < len(test_data_t):
+        batch = get_batch(test_data_t, iTest, batch_size)
+        iTest += batch_size
+        test1_inputs, test2_inputs, test_labels = batch
+        if use_gpu:
+            test_labels = test_labels.cuda()
+    
+        model.batch_size = len(test_labels)
+        model.hidden = model.init_hidden()
+        with torch.no_grad():
+            output = model(test1_inputs, test2_inputs)
+        
+        # calc testing acc
+        similarity_scores.extend(output.data.cpu().numpy())
+        trues.extend(test_labels.cpu().numpy())
+    
+    
+    trues = np.array(trues)
+    
+
+
+    max_Acc = -np.inf
+    for similarity_threshold_int in range(-5, 10):
+        similarity_threshold = similarity_threshold_int/10
+        # Classify code pairs based on the similarity score and threshold
+        predicted_labels = (np.array(similarity_scores) > similarity_threshold)
+        acc = 1-np.sum(np.abs(predicted_labels-np.transpose(trues)))/trues.shape[0]
+        P, R, F1, _ = precision_recall_fscore_support(predicted_labels, trues, average='binary', pos_label=1)
+    
+    
+        if acc > max_Acc:
+            max_Acc = acc
+            best_similarity_threshold = similarity_threshold
+
+    predicted_labels = np.array(similarity_scores) > best_similarity_threshold
+    p, r, f, _ = precision_recall_fscore_support(trues, predicted_labels, average='binary')
+    acc = 1-np.sum(np.abs(predicted_labels-np.transpose(trues)))/trues.shape[0]
+    print("F1=%.3f, P=%.3f, R=%.3f, A=%.3f for similarity threshold %0.2f" % (f, p, r, acc, best_similarity_threshold))
+
+
+    max_F1 = -np.inf
+    for similarity_threshold_int in range(-5, 10):
+        similarity_threshold = similarity_threshold_int/10
+        # Classify code pairs based on the similarity score and threshold
+        predicted_labels = (np.array(similarity_scores) > similarity_threshold)
+        acc = 1-np.sum(np.abs(predicted_labels-np.transpose(trues)))/trues.shape[0]
+        P, R, F1, _ = precision_recall_fscore_support(predicted_labels, trues, average='binary', pos_label=1)
+    
+    
+        if F1 > max_F1:
+            max_F1 = F1
+            best_similarity_threshold = similarity_threshold
+            
+    predicted_labels = np.array(similarity_scores) > best_similarity_threshold
+    p, r, f, _ = precision_recall_fscore_support(trues, predicted_labels, average='binary')
+    acc = 1-np.sum(np.abs(predicted_labels-np.transpose(trues)))/trues.shape[0]
+    print("F1=%.3f, P=%.3f, R=%.3f, A=%.3f for similarity threshold %0.2f" % (f, p, r, acc, best_similarity_threshold))
+
+
+    sys.stdout.flush()
+    model.train()
+    return f, similarity_scores
